1723bd3c88c6ca3062e6e6abdf0b96431c5b3dd1
Edaurd Kosyan <edkosyan2000@gmail.com>
2025-01-24 16:22:37 +0400

    add validators

--- src/routers/bets.router.ts
+++ src/routers/bets.router.ts
@@ -1,7 +1,10 @@
 import { Router } from "express";
 import { checkAuth } from "../utils/checkAuth";
 import { httpCreateBet } from "../controllers/betsController";
+import { createBetValidator } from "../utils/betsValidatiors";
+import { handleValidationErrors } from "../utils/handleValidationErrors";
 
 export const betsRouter = Router();
 
-betsRouter.post("/", checkAuth, httpCreateBet)
+betsRouter.post("/", checkAuth,  createBetValidator,
+  handleValidationErrors, httpCreateBet)
---
+++ src/utils/betsValidatiors.ts
@@ -0,0 +1,13 @@
+import { body } from "express-validator";
+import { MAX_BET } from "../constants";
+
+export const createBetValidator = [
+  body("amount")
+    .isNumeric()
+    .withMessage("The value must be a number.")
+    .custom((value) => value > 0 && value < MAX_BET)
+    .withMessage(
+      `The bet amount must be greater than 0 and less than ${MAX_BET}.`
+    ),
+  body("events").isArray(),
+];
