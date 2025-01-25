d8c8cdac733afe96a824e8705d9c346e638ba173
Edaurd Kosyan <edkosyan2000@gmail.com>
2025-01-25 15:12:48 +0400

    add sorting

--- src/controllers/eventsController.ts
+++ src/controllers/eventsController.ts
@@ -9,6 +9,13 @@ export const httpGetEvents = async (
 ): Promise<any> => {
   try {
     const { data } = await eventsApi.getEvents();
+    data.sort((a: any, b: any) => {
+      const f = new Date(a.commence_time).getTime()
+      const s = new Date(b.commence_time).getTime()
+      console.log(f)
+      console.log(s)
+      return f - s
+    })
     return res.status(200).json(data);
   } catch (e) {
     return res.status(500).json(["Internal server error"]);
